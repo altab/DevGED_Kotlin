645a54feaeaee18e25f4e374748a766707a37e62
Philippe Cohen <altab92@gmail.com>
2019-06-07 00:37:48 +0200

    create ressource object in fragment

--- app/src/main/java/fr/pcohen/devged_kotlin/business/Ressource.kt
+++ app/src/main/java/fr/pcohen/devged_kotlin/business/Ressource.kt
@@ -2,18 +2,22 @@ package fr.pcohen.devged_kotlin.business
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
+import org.threeten.bp.LocalDateTime
 
 @Entity
-data class Search(
+data class Ressource(
 
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int?,
 
     val title: String,
     val res: String,
-    val date: Date,
+    val date: LocalDateTime,
     val tags: List<String>,
     val type: String
 
 )
+
+enum class Type {
+    URL, TEXT
+}
--- app/src/main/java/fr/pcohen/devged_kotlin/ui/ressource/RessourceFragment.kt
+++ app/src/main/java/fr/pcohen/devged_kotlin/ui/ressource/RessourceFragment.kt
@@ -1,13 +1,20 @@
 package fr.pcohen.devged_kotlin.ui.ressource
 
+import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import fr.pcohen.devged_kotlin.base.BaseFragment
+import fr.pcohen.devged_kotlin.business.Ressource
+import fr.pcohen.devged_kotlin.business.Type
 import fr.pcohen.devged_kotlin.databinding.FragmentRessourceBinding
+import org.threeten.bp.LocalDateTime.now
 import javax.inject.Inject
 
 class RessourceFragment : BaseFragment() {
@@ -28,12 +35,67 @@ class RessourceFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        passwordChangeViewModel = ViewModelProviders.of(this, viewModelFactory).get(RessourceViewModel::class.java)
 
+        var type: String? = null
+
+        passwordChangeViewModel = ViewModelProviders.of(this, viewModelFactory).get(RessourceViewModel::class.java)
         binding = FragmentRessourceBinding.inflate(inflater, container, false)
+
+        val adapter = ArrayAdapter(context!!, R.layout.simple_spinner_item, listOf("URL", "TEXT"))
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+
+        val title = binding.textinputTitle.text
+        val ressource = binding.editTextRessource.text
+        val listTags = binding.TextinputTags.text
+
+        binding.spinnerType.adapter = adapter
+        binding.spinnerType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val item = adapter.getItem(position)
+                Log.d(TAG, "item selected : $item")
+                when (item) {
+                    Type.TEXT.toString() -> type = item
+                    Type.URL.toString() -> type = item
+                }
+            }
+        }
+
+        binding.buttonSave.setOnClickListener {
+            val objetRessource = Ressource(
+                null,
+                title.toString(),
+                ressource.toString(),
+                now(),
+                transformStringToTags(listTags.toString()),
+                type!!
+            )
+        }
 
         return binding.root
     }
 
+    private fun transformStringToTags(listTags: String): List<String> {
+        return listTags.split(",").map { it -> it.trim() }
+    }
+
 
 }
+
+/*
+<com.google.android.material.textfield.TextInputLayout
+android:layout_width="0dp"
+android:layout_height="wrap_content"
+app:layout_constraintStart_toStartOf="@+id/textInputLayout"
+app:layout_constraintEnd_toEndOf="@+id/textInputLayout"
+app:layout_constraintTop_toBottomOf="@+id/spinner_type"
+android:id="@+id/intextinput_ressource"
+android:layout_margin="10dp">
+
+<com.google.android.material.textfield.TextInputEditText
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="@string/url"/>
+
+</com.google.android.material.textfield.TextInputLayout>*/
